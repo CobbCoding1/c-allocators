77cc218406a9210cb92eda0ce982eeff59b923c9
CobbCoding <rircobb@gmail.com>
2023-12-02 15:16:56 -0500

    added linked list to arena allocator

--- arena/main.c
+++ arena/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 #include <assert.h>
 
@@ -16,25 +17,46 @@ Arena arena_init(size_t capacity) {
         .capacity = capacity,
         .size = 0,
         .data = data,
+        .next = NULL,
     };
     return arena;
 }
 
 void *arena_alloc(Arena *arena, size_t size) {
-    assert(arena->size + size <= arena->capacity);
-    uint8_t *data = &arena->data[arena->size];
-    arena->size += size;
+    assert(arena->capacity >= size);
+    Arena *current = arena;
+    while(!(current->size + size <= current->capacity)) {
+        if(current->next == NULL) {
+            Arena *next = malloc(sizeof(Arena));
+            Arena initted = arena_init(arena->capacity);
+            memcpy(next, &initted, sizeof(Arena));
+            current->next = next;
+        }
+        current = current->next;
+    }
+
+    uint8_t *data = &current->data[current->size];
+    current->size += size;
     return data;
 }
 
 void *arena_reset(Arena *arena) {
-    arena->size = 0;
+    Arena *current = arena;
+    while(current->next != NULL) {
+        current->size = 0;
+        current = current->next;
+    }
 }
 
 void *arena_free(Arena *arena) {
-    arena->capacity = 0;
-    arena->size = 0;
-    free(arena->data);
+    Arena *current = arena;
+    while(current->next != NULL) {
+        current->capacity = 0;
+        current->size = 0;
+        free(current->data);
+        current = current->next;
+    }
+    arena->next = NULL;
 }
 
 void print_arena(const Arena *arena) {
@@ -46,7 +68,10 @@ int main() {
     void *ptr = arena_alloc(&arena, 18);
     void *ptr2 = arena_alloc(&arena, 10);
     void *ptr3 = arena_alloc(&arena, 218);
-    void *ptr3 = arena_alloc(&arena, 1000);
-    print_arena(&arena);
+    void *ptr4 = arena_alloc(&arena, 1000);
+    void *ptr5 = arena_alloc(&arena, 1023);
+    void *ptr6 = arena_alloc(&arena, 218);
+    arena_reset(&arena);
+    print_arena(arena.next);
     arena_free(&arena);
 }
