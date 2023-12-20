ea77443d3fe3be1d1baacbc5e26261395551618b
CobbCoding <rircobb@gmail.com>
2023-12-20 16:50:37 -0500

    readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # A collection of allocators implemented in C
 
+## Quick Start
+Requirements - CC
+```sh
+cd arena
+./build.sh
+./main
+```
+
+## Currently implemented
+- [x] Arena Allocator
+- [x] Stack Allocator
+- [ ] Pool Allocator
+- [ ] Reimplement Malloc (from scratch using syscalls)
+
--- stack/main.c
+++ stack/main.c
@@ -16,7 +16,7 @@ typedef struct {
 void *custom_realloc(void *pointer, size_t size) {
     void *ptr = realloc(pointer, sizeof(byte) * size);
     if(ptr == NULL) {
-        fprintf(stderr, "error: buy more ram");
+        fprintf(stderr, "error: buy more ram\n");
         exit(1);
     }
     return ptr;
@@ -34,24 +34,24 @@ Stack stack_init(size_t capacity) {
 
 void *stack_realloc(Stack *stack, size_t capacity) {
     stack->capacity = capacity;
-    stack->data = realloc(stack->data, capacity);
+    stack->data = realloc(stack->data, sizeof(byte) * capacity);
     return stack->data;
 }
 
-// allocates the size passed + 8 more (sizeof(size_t),ALLIGNMENT)
+// allocates the size passed + 8 more (sizeof(size_t), ALLIGNMENT) because it has to store the size as well
 void *stack_push(Stack *stack, size_t size) {
-    if(stack->capacity < size || stack == NULL) {
+    if(stack->capacity < size || stack->size + size > stack->capacity || stack == NULL) {
         return NULL;
     }
     byte *data = &stack->data[stack->size];
     stack->size += size;
-    stack->data[stack->size] = size;
+    *(size_t*)stack->data = size;
     stack->size += ALIGNMENT;
     return data;
 }
 
 int stack_pop(Stack *stack) {
-    if(stack->size < 8 || stack == NULL) {
+    if(stack->size < ALIGNMENT || stack == NULL) {
         return 1;
     }
     size_t amount = *(size_t*)&stack->data[stack->size - ALIGNMENT];
@@ -82,7 +82,6 @@ void stack_print(const Stack *stack) {
 int main(void) {
     Stack stack = stack_init(1000);
     stack_print(&stack);
-    stack_realloc(&stack, 2000);
     int *value = stack_push(&stack, 12);
     char *str = stack_push(&stack, sizeof(char) * 15);
     strncpy(str, "Hello, world!\n", 15);
